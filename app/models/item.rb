d458568b67af9e2bba10d3c9f0ab162807ea0fe7
0zone-k <mpwjeljpm@gmail.com>
2024-11-15 14:42:01 +0900

    単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
   validates :title, presence: true
   validates :introduction, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :postage_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shopping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :situation_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :user, presence: true
+  validates :user_id, presence: true
 
   has_one_attached :image
   belongs_to :user
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title             { 'サンプルタイトル' }
+    introduction      { 'サンプル説明' }
+    price             { 5000 }
+    prefecture_id { 2 }
+    postage_id        { 2 }
+    shopping_date_id  { 2 }
+    category_id       { 2 }
+    situation_id      { 2 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
